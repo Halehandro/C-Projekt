06ed223ce2bacbf6f037f7032d6c52537e0a3989
Domagoj Halusek <dohaluse@vsite.hr>
2018-02-19 20:48:23 +0100

    Initial commit

---
+++ ListaProcesa/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace ListaProcesa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Osvjezi = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label1.Location = new System.Drawing.Point(140, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AKTIVNI PROCESI";
+            // 
+            // listView1
+            // 
+            this.listView1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(109, 81);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(377, 477);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "PROCESS ";
+            this.columnHeader1.Width = 132;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "CPU USAGE ( % )";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 110;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "RAM USAGE ( MB )";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 131;
+            // 
+            // Osvjezi
+            // 
+            this.Osvjezi.Enabled = true;
+            this.Osvjezi.Interval = 1000;
+            this.Osvjezi.Tick += new System.EventHandler(this.Osvjezi_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Chocolate;
+            this.ClientSize = new System.Drawing.Size(580, 570);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Lista procesa";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Timer Osvjezi;
+    }
+}
+
---
+++ ListaProcesa/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Diagnostics.PerformanceData;
+using System.Threading;
+
+namespace ListaProcesa
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            LoadProcesses();
+        }
+
+        private void LoadProcesses()
+        {
+            listView1.Items.Clear();
+
+            Process[] Aktivni = Process.GetProcesses();
+
+            foreach (Process proc in Aktivni)
+            {
+                PerformanceCounter PROCcpu =
+                    new PerformanceCounter("Process", "% Processor Time", proc.ProcessName, true);
+
+                ListViewItem item = new ListViewItem((proc.ProcessName).ToString());
+                item.SubItems.Add(PROCcpu.NextValue() + " " + "%");
+                item.SubItems.Add(((proc.WorkingSet64/ 1024) / 1024).ToString() + " " + "MB" );
+                listView1.Items.Add(item);
+            }
+
+        }
+
+        private void Osvjezi_Tick(object sender, EventArgs e)
+        {
+            LoadProcesses();
+        }
+    }
+}
